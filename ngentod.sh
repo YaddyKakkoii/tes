2b6a8791f1fd78e512e511ee350486b64d9bbd3a
nonamez <127028273+YaddyKakkoii@users.noreply.github.com>
2024-07-03 11:06:54 +0700

    Update ngentod.sh

--- ngentod.sh
+++ ngentod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://raw.githubusercontent.com/YaddyKakkoii/tes/main/entod.sh > /usr/bin/ngentod; bash /usr/bin/ngentod; rm /usr/bin/ngentod￼Enter
+curl -s https://raw.githubusercontent.com/YaddyKakkoii/tes/main/entod.sh > /usr/bin/ngentod; bash /usr/bin/ngentod; rm /usr/bin/ngentod
